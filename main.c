8d0ef4313dc84dcd821730ae897e3739d0f36104
KeiraVenic <karrelvpc13@gmail.com>
2025-06-06 00:11:30 +0700

    Menambahkan section untuk Kalkulasi Harga

--- main.c
+++ main.c
@@ -60,6 +60,7 @@ void menuMasuk(int a);
 void tambahMotor();
 void stockMotor();
 void dataPenyewa();
+void kalkulasiHarga();
 
 int main() {
     system("color F0");
@@ -373,6 +374,68 @@ void dataPenyewa() {
     printf("Data penyewa berhasil disimpan!\n");
 }
 
+void kalkulasiHarga() {
+    clearScreen();
+    if (jumlahPenyewa == 0) {
+        gotoxy(25, 10);
+        printf("Belum ada data penyewa.\n");
+        return;
+    }
+
+    gotoxy(25, 5);
+    printf("--- KALKULASI HARGA SEWA ---");
+
+    // Display all renters
+    for (int i = 0; i < jumlahPenyewa; i++) {
+        gotoxy(25, 7 + (i*4));
+        printf("Penyewa ke-%d", i + 1);
+        gotoxy(25, 8 + (i*4));
+        printf("Nama Penyewa  : %s", daftarPenyewa[i].namaPenyewa);
+        gotoxy(25, 9 + (i*4));
+        printf("Motor Disewa  : %s (%d hari)", daftarPenyewa[i].namaMotor, daftarPenyewa[i].durasiSewa);
+        gotoxy(25, 10 + (i*4));
+        printf("-----------------------------------");
+    }
+
+    int pilihan;
+    gotoxy(25, 11 + (jumlahPenyewa*4));
+    printf("Pilih penyewa untuk kalkulasi (nomor): ");
+    scanf("%d", &pilihan);
+    getchar();
+
+    if (pilihan < 1 || pilihan > jumlahPenyewa) {
+        gotoxy(25, 13 + (jumlahPenyewa*4));
+        printf("Pilihan tidak valid.\n");
+        return;
+    }
+
+    // Find the motor information
+    float hargaPerHari = 0;
+    for (int i = 0; i < jumlahMotor; i++) {
+        if (strcmp(daftarMotor[i].nama, daftarPenyewa[pilihan-1].namaMotor) == 0) {
+            hargaPerHari = daftarMotor[i].hargaPerHari;
+            break;
+        }
+    }
+
+    float totalHarga = hargaPerHari * daftarPenyewa[pilihan-1].durasiSewa;
+
+    clearScreen();
+    gotoxy(25, 5);
+    printf("--- HASIL KALKULASI HARGA ---");
+    gotoxy(25, 7);
+    printf("Nama Penyewa  : %s", daftarPenyewa[pilihan-1].namaPenyewa);
+    gotoxy(25, 8);
+    printf("Motor Disewa  : %s", daftarPenyewa[pilihan-1].namaMotor);
+    gotoxy(25, 9);
+    printf("Durasi Sewa   : %d hari", daftarPenyewa[pilihan-1].durasiSewa);
+    gotoxy(25, 10);
+    printf("Harga per Hari: Rp %.2f", hargaPerHari);
+    gotoxy(25, 11);
+    printf("Total Harga   : Rp %.2f", totalHarga);
+    gotoxy(25, 13);
+    printf("===================================");
+}
 
 void loginUser() {
     clearScreen();
